2f4bb8dd9affa3690b8acd40324ed5ef48546583
carvalhocaio <caiocarvalho.py@gmail.com>
2023-02-23 00:35:09 -0400

    create a cli for the python weather app

--- weather.py
+++ weather.py
@@ -1,4 +1,25 @@
+import argparse
 from configparser import ConfigParser
+
+
+def read_user_cli_args():
+    """Handle the CLI user interactions.
+
+
+    Returns:
+        argparse.Namespace: Populated namespace object
+    """
+    parser = argparse.ArgumentParser(
+        description="gets weather and temperature information for a city"
+    )
+    parser.add_argument("city", nargs="+", type=str, help="enter the city name")
+    parser.add_argument(
+        "-i",
+        "--imperial",
+        action="store_true",
+        help="display the temperature in imperial units",
+    )
+    return parser.parse_args()
 
 
 def _get_api_key():
@@ -12,3 +33,8 @@ def _get_api_key():
     config = ConfigParser()
     config.read("secrets.ini")
     return config["openweather"]["api_key"]
+
+
+if __name__ == "__main__":
+    user_args = read_user_cli_args()
+    print(user_args.city, user_args.imperial)
